2aa52e35ab906d00aed2a2729c0f4dcbd6004063
Madelyne Velasco <m_velasco93@hotmail.com>
2018-08-20 12:49:04 -0500

    Time of query

--- gifs/views.py
+++ gifs/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import HttpResponse
 from django.template import loader
 from .models import Gif
+import time
 
 def index(request):
+	start_time = time.time()
 	most_viewed_gifs = Gif.objects.order_by('-contador')[:10]
+	end_time = (time.time() - start_time)
 	template = loader.get_template('index.html')
 	context = {
 		'most_viewed_gifs': most_viewed_gifs,
+		'end_time': end_time,
 	}
 	return HttpResponse(template.render(context, request))
